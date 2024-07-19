e03a7edb4e6d14878b880d2a29a0961643cc00d1
huang-jl <1046678590@qq.com>
2024-07-19 21:20:17 +0800

    Add lazygit with toggleterm
    Press <leader>gg will open a lazygit float term.

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -6,7 +6,7 @@ end
 toggleterm.setup({
 	size = 20,
 	open_mapping = [[<c-\>]],
-	hide_numbers = true,
+	hide_numbers = false,
 	shade_filetypes = {},
 	shade_terminals = true,
 	shading_factor = 2,
@@ -29,7 +29,7 @@ toggleterm.setup({
 function _G.set_terminal_keymaps()
   local opts = {noremap = true}
   vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
+  -- vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
@@ -39,31 +39,50 @@ end
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
 local Terminal = require("toggleterm.terminal").Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+local lazygit = Terminal:new({
+  cmd = "lazygit",
+  hidden = true,
+  on_create = function(term)
+    vim.api.nvim_buf_del_keymap(term.bufnr, "t", "<Esc>")
+    vim.api.nvim_buf_set_keymap(term.bufnr, "t", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+  end,
+})
 
 function _LAZYGIT_TOGGLE()
 	lazygit:toggle()
 end
 
-local node = Terminal:new({ cmd = "node", hidden = true })
+-- local node = Terminal:new({ cmd = "node", hidden = true })
+--
+-- function _NODE_TOGGLE()
+-- 	node:toggle()
+-- end
+--
+-- local ncdu = Terminal:new({ cmd = "ncdu", hidden = true })
+--
+-- function _NCDU_TOGGLE()
+-- 	ncdu:toggle()
+-- end
 
-function _NODE_TOGGLE()
-	node:toggle()
-end
-
-local ncdu = Terminal:new({ cmd = "ncdu", hidden = true })
-
-function _NCDU_TOGGLE()
-	ncdu:toggle()
-end
-
-local htop = Terminal:new({ cmd = "htop", hidden = true })
+local htop = Terminal:new({
+  cmd = "htop",
+  hidden = true,
+  on_create = function(term)
+    vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<leader>q", "<cmd>close<CR>", {noremap = true, silent = true})
+  end,
+})
 
 function _HTOP_TOGGLE()
 	htop:toggle()
 end
 
-local python = Terminal:new({ cmd = "python", hidden = true })
+local python = Terminal:new({
+  cmd = "python3",
+  hidden = true,
+  on_create = function(term)
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+  end,
+})
 
 function _PYTHON_TOGGLE()
 	python:toggle()
