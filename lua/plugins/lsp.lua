e1e9885f0458aa1628467193ff7ac8e3d5f87da8
huang-jl <1046678590@qq.com>
2024-11-26 15:50:36 +0800

    Add black for python lint

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,7 +2,6 @@ return {
   { "neovim/nvim-lspconfig" }, -- enable LSP
   {
     "williamboman/mason.nvim",
-    build = ":MasonUpdate", -- :MasonUpdate updates registry contents
   },
   { "williamboman/mason-lspconfig.nvim" },
   { 'simrat39/rust-tools.nvim' }, -- for rust
--- lua/user/lsp/mason.lua
+++ lua/user/lsp/mason.lua
@@ -118,6 +118,7 @@ local servers = {
   "clangd",
   "gopls",
   "rust_analyzer",
+  "black",
 }
 
 local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
